a8b04795131c85c98dee8747e8aba9bbe43db501
Abdulaziz Kikanga <abdulazizkikanga@gmail.com>
2026-03-04 06:29:48 +0300

    Create 06_deduplicate_tables.sql

---
+++ bigquery/seed_data/06_deduplicate_tables.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE TABLE `zora-retention-stack.zora_prod.wallet_registry` AS
+SELECT DISTINCT *
+FROM `zora-retention-stack.zora_prod.wallet_registry`;
+
+CREATE OR REPLACE TABLE `zora-retention-stack.zora_prod.mint_events` AS
+SELECT DISTINCT *
+FROM `zora-retention-stack.zora_prod.mint_events`;

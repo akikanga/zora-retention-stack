8743aa363935e9714f0c15a0e4d7687220d87216
Abdulaziz Kikanga <abdulazizkikanga@gmail.com>
2026-03-04 10:29:43 +0300

    Update 06_create_hightouch_planner.sql

--- bigquery/schema/06_create_hightouch_planner.sql
+++ bigquery/schema/06_create_hightouch_planner.sql
@@ -2,3 +2,7 @@ CREATE SCHEMA IF NOT EXISTS `zora-retention-stack.hightouch_planner`;
 GRANT `roles/bigquery.dataViewer`, `roles/bigquery.dataEditor`
 ON SCHEMA `zora-retention-stack.hightouch_planner`
 TO "serviceAccount:hightouch-bigquery-reader@zora-retention-stack.iam.gserviceaccount.com";
+CREATE SCHEMA IF NOT EXISTS `zora-retention-stack.hightouch_audit`;
+GRANT `roles/bigquery.dataViewer`, `roles/bigquery.dataEditor`
+ON SCHEMA `zora-retention-stack.hightouch_audit`
+TO "serviceAccount:hightouch-bigquery-reader@zora-retention-stack.iam.gserviceaccount.com";
